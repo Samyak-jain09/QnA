142ee84e6d27d060209c72d5a6d864e6c06215ec
Samyak Jain <samyakjain0902@gmail.com>
2022-01-15 21:13:33 +0530

    model feild added

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,12 @@
+from pickle import TRUE
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    text=models.CharField(max_length=240)
+    date=models.DateTimeField(auto_now=TRUE)
+
+    def __str__(self):
+        return self.text[0:100]
