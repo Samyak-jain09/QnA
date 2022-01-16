90f3a2c18e20ce8860ded13b22d3a3dbe9371287
Samyak Jain <samyakjain0902@gmail.com>
2022-01-16 17:23:32 +0530

    made js file

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Post
 # Create your views here.
 
@@ -8,3 +12,27 @@ class HomePageView(ListView):
     model=Post
     context_object_name="posts"
     queryset=Post.objects.all().order_by('-id')[0:30]
+
+
+class PostDetailView(DetailView):
+    http_method_names= ("get")
+    template_name="feed/detail.html"
+    model=Post
+    context_block_name="post"
+    
+
+class CreateNewPost( LoginRequiredMixin,CreateView):
+    model= Post
+    template_name= "feed/create.html"
+    fields=['text']
+    success_url="/"
+
+    def dispatch(self, request, *args, **kwargs) :
+        self.request=request
+        return super().dispatch(request,*args,**kwargs)
+
+    def form_valid(self,form):
+        obj= form.save(commit=False)
+        obj.author=self.request.user
+        obj.save()
+        return super().form_valid(form)
