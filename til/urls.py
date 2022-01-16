e2ab55c0cb3c1d467775823c1f3b5b2d0c713a07
Samyak Jain <samyakjain0902@gmail.com>
2022-01-16 15:35:18 +0530

    profile pic setup

--- til/urls.py
+++ til/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from argparse import Namespace
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.urls import re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from feed import urls as feed_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("",include(feed_urls,namespace="feed")),
+    re_path("", include("allauth.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
