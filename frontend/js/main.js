c01c2b69fc00ce0e9795e68f10076dcb8dd38342
Samyak-jain09 <samyakjain0902@gmail.com>
2022-02-23 00:23:45 +0530

    Minor changes

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -28,68 +28,75 @@ $.ajaxSetup({
     },
 });
 
-$(document).on("click", ".js-toggle-modal", function(e) {
+$(document).on("click", ".js-toggle-modal", function (e) {
     e.preventDefault()
     $(".js-modal").toggleClass("hidden")
 })
-.on("click", ".js-submit", function(e) {
-    e.preventDefault()
-    const text = $(".js-post-text").val().trim()
-    // var form_upload= $(".js-post-form")[0];
-    // var image=new FormData(form_upload);
-    
-    const $btn = $(this)
+    .on("click", ".js-submit", function (e) {
+        e.preventDefault()
+        const text = $(".js-post-text").val().trim()
+        // var form_upload= $(".js-post-form")[0];
+        // var image=new FormData(form_upload);
 
-    if(!text.length) {
-        return false
-    }
+        const $btn = $(this)
 
-    $btn.prop("disabled", true).text("Posting!")
-    $.ajax({
-        type: 'POST',
-        url: $(".js-post-text").data("post-url"),
-        data: {
-            text: text,
-            // image: image
-        },
-        success: (dataHtml) => {
-            $(".js-modal").addClass("hidden");
-            $("#posts-container").prepend(dataHtml);
-            $btn.prop("disabled", false).text("New Post");
-            $(".js-post-text").val('')
-        },
-        error: (error) => {
-            console.warn(error)
-            $btn.prop("disabled", false).text("Error");
+        if (!text.length) {
+            return false
         }
-    });
-})
-.on("click", ".js-follow", function(e) {
-    
-    e.preventDefault();
-    const action = $(this).attr("data-action")
 
-    $.ajax({
-        type: 'POST',
-        url: $(this).data("url"),
-        data: {
-            action: action,
-            username: $(this).data("username"),
-        },
-        success: (data) => {
-            $(".js-follow-text").text(data.wording)
-            if(action == "follow") {
-                // Change wording to unfollow
-                console.log("DEBUG", "unfollow")
-                $(this).attr("data-action", "unfollow")
-            } else {
-                // The opposite
-                console.log("DEBUG", "follow")
-                $(this).attr("data-action", "follow")
+        $btn.prop("disabled", true).text("Posting!")
+        $.ajax({
+            type: 'POST',
+            url: $(".js-post-text").data("post-url"),
+            data: {
+                text: text,
+                // image: image
+            },
+            success: (dataHtml) => {
+                $(".js-modal").addClass("hidden");
+                $("#posts-container").prepend(dataHtml);
+                $btn.prop("disabled", false).text("New Post");
+                $(".js-post-text").val('')
+            },
+            error: (error) => {
+                console.warn(error)
+                $btn.prop("disabled", false).text("Error");
             }
-        },
-        error: (error) => {
-            console.warn(error)
-        }
-    });
-})
+        });
+    })
+    .on("click", ".js-follow", function (e) {
+
+        e.preventDefault();
+        const action = $(this).attr("data-action")
+
+        $.ajax({
+            type: 'POST',
+            url: $(this).data("url"),
+            data: {
+                action: action,
+                username: $(this).data("username"),
+            },
+            success: (data) => {
+                $(".js-follow-text").text(data.wording)
+                if (action == "follow") {
+                    // Change wording to unfollow
+                    console.log("DEBUG", "unfollow")
+                    $(this).attr("data-action", "unfollow")
+                } else {
+                    // The opposite
+                    console.log("DEBUG", "follow")
+                    $(this).attr("data-action", "follow")
+                }
+            },
+            error: (error) => {
+                console.warn(error)
+            }
+        });
+    })
+
+
+
+    
+
+    
+    
